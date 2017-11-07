5be9848069d3f4e47274f9d6bb8a5e9f1cb616bd
Misty Malkasian <andrewandmisty@gmail.com>
2017-11-06 21:25:33 -0600

    Connected SQL table to project and am able to easily edit it at will

---
+++ Humane_Society/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Humane_Society
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClassesDataContext DC = new DataClassesDataContext(Properties.Settings.Default.HumaneSocietyConnectionString);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (DC.DatabaseExists()) AnimalsDatagrid.ItemsSource = DC.Animals;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DC.SubmitChanges();
+        }
+    }
+}
